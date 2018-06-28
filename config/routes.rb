d606ba9b120f62941b93cef9119e88de4479f764
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-28 17:12:53 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-  post '/signout' => 'sessions#destroy'
+  delete '/signout' => 'sessions#destroy'
   post "/rides/new" => 'rides#new'
 
   resources :users
