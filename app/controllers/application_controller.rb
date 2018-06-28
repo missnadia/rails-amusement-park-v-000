a4527bc8983060836e3128724ce31b5b116c4dda
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-28 17:25:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login, except: [:home, :new, :create]
-  before_action :current_user
+  helper_method :current_user
 
   private
 
