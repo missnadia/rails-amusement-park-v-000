85359e5b62b9aeec58220062825ae8fa2606a423
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-28 18:03:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to signin_path
+      redirect_to signin_url
     end
   end
 
