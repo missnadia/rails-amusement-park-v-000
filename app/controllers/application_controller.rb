a06157dea0631ae45a095675a142da0490392122
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-28 17:50:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   private
 
   def logged_in?
-    current_user.id != nil
+    !!current_user
   end
 
   def current_user
-    @user = (User.find_by(id: session[:user_id]) || User.new)
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def require_login
